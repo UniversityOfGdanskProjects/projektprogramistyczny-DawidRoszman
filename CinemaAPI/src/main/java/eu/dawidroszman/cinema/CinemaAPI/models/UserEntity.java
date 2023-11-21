766963d763dc8b621d91ae288a378790525f794e
Dawid Roszman <dawidroszman@gmail.com>
2023-11-21 11:39:58 +0100

    Deleted unused imports

--- CinemaAPI/src/main/java/eu/dawidroszman/cinema/CinemaAPI/models/UserEntity.java
+++ CinemaAPI/src/main/java/eu/dawidroszman/cinema/CinemaAPI/models/UserEntity.java
@@ -1,16 +1,8 @@
 package eu.dawidroszman.cinema.CinemaAPI.models;
 
-import eu.dawidroszman.cinema.CinemaAPI.enums.Role;
 import lombok.*;
-import org.springframework.data.neo4j.core.schema.GeneratedValue;
 import org.springframework.data.neo4j.core.schema.Id;
 import org.springframework.data.neo4j.core.schema.Node;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.core.userdetails.UserDetails;
-
-import java.util.Collection;
-import java.util.List;
 
 @Data
 @Builder
