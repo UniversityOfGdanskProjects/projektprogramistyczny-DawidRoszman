8580dd0416bf21c9e2b5a0b3f05775c937a64390
Dawid Roszman <d.roszman.682@studms.ug.edu.pl>
2024-01-12 11:49:43 +0100

    add movie check if movie already exists

--- CinemaAPI/src/main/java/eu/dawidroszman/cinema/CinemaAPI/services/MovieService.java
+++ CinemaAPI/src/main/java/eu/dawidroszman/cinema/CinemaAPI/services/MovieService.java
@@ -2,45 +2,55 @@ package eu.dawidroszman.cinema.CinemaAPI.services;
 
 import eu.dawidroszman.cinema.CinemaAPI.models.MovieEntity;
 import eu.dawidroszman.cinema.CinemaAPI.repositories.MovieRepository;
+
+import org.apache.el.stream.Optional;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
 public class MovieService {
-    private final MovieRepository movieRepository;
+  private final MovieRepository movieRepository;
 
-    public MovieService(MovieRepository movieRepository) {
-        this.movieRepository = movieRepository;
-    }
+  public MovieService(MovieRepository movieRepository) {
+    this.movieRepository = movieRepository;
+  }
 
-    public List<MovieEntity> getAllMovies() {
-        return movieRepository.findAllMovies();
-    }
+  public List<MovieEntity> getAllMovies() {
+    return movieRepository.findAllMovies();
+  }
 
-    public MovieEntity getMovieByTitle(String title) {
-        return movieRepository.findByTitle(title).orElseThrow();
-    }
+  public MovieEntity getMovieByTitle(String title) {
+    return movieRepository.findByTitle(title).orElseThrow();
+  }
 
-    public List<MovieEntity> searchMoviesByTitle(String title) {
-        return movieRepository.findByTitleContaining(title);
-    }
+  public List<MovieEntity> searchMoviesByTitle(String title) {
+    return movieRepository.findByTitleContaining(title);
+  }
 
-    public void addMovie(MovieEntity movie) {
-        movieRepository.save(movie);
-    }
+  public void addMovie(MovieEntity movie) {
+    Optional<MovieEntity> movieOptional = movieRepository.findByTitle(movie.getTitle());
+    movieOptional.ifPresentOrElse(
+        m -> {
+          throw new RuntimeException("Movie already exists");
+        },
+        () -> {
+          movieRepository.save(movie);
+        });
+  }
 
-    public void deleteMovie(String movieTitle) {
-        MovieEntity movie = movieRepository.findByTitle(movieTitle).orElseThrow(() -> new RuntimeException("Movie not found"));
-        movieRepository.delete(movie);
-    }
+  public void deleteMovie(String movieTitle) {
+    MovieEntity movie = movieRepository.findByTitle(movieTitle)
+        .orElseThrow(() -> new RuntimeException("Movie not found"));
+    movieRepository.delete(movie);
+  }
 
-    public void updateMovie(MovieEntity movie) {
-        String title = movie.getTitle();
-        String description = movie.getDescription();
-        Integer released = movie.getReleased();
-        String imageUrl = movie.getImageUrl();
-        String trailer = movie.getTrailer();
-        movieRepository.modifyMovie(title, description, released, imageUrl, trailer);
-    }
+  public void updateMovie(MovieEntity movie) {
+    String title = movie.getTitle();
+    String description = movie.getDescription();
+    Integer released = movie.getReleased();
+    String imageUrl = movie.getImageUrl();
+    String trailer = movie.getTrailer();
+    movieRepository.modifyMovie(title, description, released, imageUrl, trailer);
+  }
 }
