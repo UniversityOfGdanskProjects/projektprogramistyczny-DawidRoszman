db5d42eb71d089f1d4bb62d0a93e4702b81166a1
Dawid Roszman <dawidroszman@gmail.com>
2024-01-21 21:07:52 +0100

    Refactor CheckIfIsLoggedIn component

--- cinema-frontend/src/components/CheckIfIsLoggedIn.tsx
+++ cinema-frontend/src/components/CheckIfIsLoggedIn.tsx
@@ -1,15 +1,23 @@
 "use client";
-import { useToken } from '@/app/components/TokenContext';
-import Link from 'next/link';
-import React from 'react';
+import { useToken } from "@/app/components/TokenContext";
+import Link from "next/link";
+import React from "react";
 
-const CheckIfIsLoggedIn = ({ children }: { children: React.ReactNode; }) => {
+const CheckIfIsLoggedIn = ({ children }: { children: React.ReactNode }) => {
   const token = useToken();
+  console.log(token);
   if (token === null) return <div>Unauthorized</div>;
-  if (token.token === "") return <div className='grid place-items-center pt-5'>
-    <div className="text-5xl text-center p-7">You need to be logged in to buy a ticket</div>
-    <Link href="/login" className="btn btn-primary">Login</Link>
-  </div>;
+  if (token.token === null || token.token === "" || token.token === undefined)
+    return (
+      <div className="grid place-items-center pt-5">
+        <div className="text-5xl text-center p-7">
+          You need to be logged in to buy a ticket
+        </div>
+        <Link href="/login" className="btn btn-primary">
+          Login
+        </Link>
+      </div>
+    );
   return <>{children}</>;
 };
 
